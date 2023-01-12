0f5cf428cfe1caa92ff88f78b59f1e35017abb04
Langer, Jacek <jacek.langer@tu-darmstadt.de>
2023-01-12 16:38:15 +0100

    generate and highlight cells

--- canvas-controller.js
+++ canvas-controller.js
@@ -9,19 +9,17 @@ autors: Jacek Langer, Nils Jürgensen, Thorsten Grave
 //  Consts
 // --------------------------------
 
-const cells = 10;
-const size = 600;
-const step = size / cells;
-const gridLineWidth = 0.2;
+const size = 600;	// size of the canvas 
+let cells = getCells()	// the amount of cells in a single row, total cell count is cells squared
 const gridcolor = "#2D1F45";
 const activeCellColor = "#FFE200";
 const highlightCellColor = "#9D7E00";
 const clearColor = "#1B0B36";
-const radius = 7;
-const margin = 5;
-const table = document.querySelector("#node-table");
-const canvas = document.querySelector("#routeCanvas");
-const ctx = canvas.getContext("2d");
+const radius = 7;	// corner radius of for the cells
+const margin = 10;	// margin at wich cells are placed
+const table = document.querySelector("#node-table");	// the table of existing nodes
+const canvas = document.querySelector("#routeCanvas");	// canvas
+const ctx = canvas.getContext("2d");	// 2d context
 // event names
 const enter = "mouseenter";
 const move = "mousemove";
@@ -31,9 +29,12 @@ const up = "mouseup";
 const click = "click";
 
 // global variables
-let rect = canvas.getBoundingClientRect();
+let grid
+let step	// the step size at which a single cell is placed
+let offset  // the offset at which the line begins
+let rect = canvas.getBoundingClientRect();		// bounding rect for the canvas to get the offset resulting from padding
 let isDebug = false
-let possibleStartpoints = [];
+let possibleStartpoints = [];	// list of possible start points. this points will be shown in a highlighted color
 
 // --------------------------------
 //  setup
@@ -49,15 +50,36 @@ let cellOpts = getCellOpts()
 window.onresize = ()=>{
 	updateCanvasSize()
 	updateTableSize()
+	setStepsize()
 	cellOpts = getCellOpts()
 }
 
+function setStepsize(){
+	step = size / cells;
+	offset = step /2
+	
+}
 
 // generate a map of cells that are initialized with null.
+grid = Array(cells)
+	.fill(null)
+	.map(() => Array(cells).fill(null));
 
-let grid = Array(cells)
-.fill(null)
-.map(() => Array(cells).fill(null));
+function reset(){
+	cells = getCells();
+	grid = Array(cells)
+		.fill(null)
+		.map(() => Array(cells).fill(null));
+	
+	clearTable();
+	setStepsize()
+	cellOpts = getCellOpts()
+	updateCanvas()
+}
+
+function getCells(){
+	return parseInt(document.querySelector('[name="number"]').value);
+}
 
 /**
  * Resize the canvas in a fashion so it consumes half the screen area.
@@ -75,10 +97,18 @@ function updateTableSize(){
 	table.height = window.innerHeight *.7
 }
 
-
-function cell(x, y) {
+/**
+ * Returns an cell object. 
+ * 
+ * todo: create a class.
+ * @param {int} x array index for x
+ * @param {int} y array index for y 
+ * @param {int} i index
+ * @returns 
+ */
+function cell(x, y,i) {
 	return {
-		index: x*y +x,
+		index: i,
 		x: x,
 		y: y,
 		bottom: y * step + step,
@@ -109,11 +139,6 @@ function cell(x, y) {
 			}else{	// add the edges
 				this.edges.add(other)
 				other.edges.add(this)
-				if(canStart){
-					enableStart();
-				}else{
-					disableStart();
-				}
 			}
 
 			center = {
@@ -125,7 +150,7 @@ function cell(x, y) {
 				y: other.top + step / 2,
 			};
 
-			gradientLine(this.center,other.center)
+			gradientLine(this.center, other.center, offset);
 		},
 		lineTo(x, y) {
 			center = {
@@ -134,18 +159,55 @@ function cell(x, y) {
 			};
 			line(center.x, center.y, x, y);
 		},
+		/**
+		 * Highlight the cell to make it distinct from other cells.
+		 */
 		highlight() {
-			if(!this.isActive)
+			if(!this.isActive){
 				drawCell(cellOpts.width * this.x, cellOpts.height * this.y, cellOpts.glow, cellOpts.glow);
-			else
+				// drawIndex(this);
+				if (possibleStartpoints.includes(this)) {
+					possibleStartpoints.pop(this);
+				}
+			} else {
+				
+				drawCell(
+					cellOpts.width * this.x,
+					cellOpts.height * this.y,
+					cellOpts.activeColor,
+					cellOpts.activeGlow
+					);
+				drawIndex(this, cellOpts.bg, cellOpts.bg);
+					
+				} 
+				// drawIndex(this);
+				ctx.fill()
+			},
+		/**
+		 * Highlight the cell as a possible startpoint.
+		 */
+		setStartpoint(){
 				drawCell(
 					cellOpts.width * this.x,
 					cellOpts.height * this.y,
 					cellOpts.activeColor,
 					cellOpts.activeGlow
 				);
-			ctx.fill()
+				
+				drawIndex(this, cellOpts.start, cellOpts.startGlow);
+				possibleStartpoints.push(this)			
+			},
+		/**
+		 * remove this cell as a startpoint
+		 */
+		removeStartpoint(){
+			if (possibleStartpoints.includes(this)) {
+				possibleStartpoints.pop(this);
+			}
 		},
+		/**
+		 * Clear the cell and reset the cell to original state
+		 */
 		clear() {
 			this.edges.forEach((e) => {
 				let x = e.x;
@@ -155,6 +217,7 @@ function cell(x, y) {
 			this.edges = new Set();
 			ctx.fillStyle = clearColor;
 			ctx.fillRect(this.left + margin, this.top + margin, step - 2 * margin, step - 2 * margin);
+			this.removeStartpoint();
 		},
 	};
 }
@@ -162,6 +225,7 @@ function cell(x, y) {
 
 /**
  * Return a cellSize object that is dependend of the current canvas size and amount of cells.
+ * todo: create class
  * @returns cellSize object with width and height size
  */
 function getCellOpts(){
@@ -170,11 +234,12 @@ function getCellOpts(){
 		height: (canvas.height - margin) / cells,
 		color: "#2D0734",
 		glow: "#592861",
-		activeColor: "#651D3A",
-		activeGlow: "#440A21",
 		bg: "#2D1F45",
 		shadow: "#0C000F",
 		start: "#4A6B1E",
+		activeColor: "#651D3A",
+		activeGlow: "#440A21",
+		startGlow: "#2C480B",
 	};
 }
 
@@ -186,6 +251,36 @@ function getCellOpts(){
 // init grid once.
 drawGrid();
 
+function highlightStartPoints(){
+	exitCode = canStart();
+	if (exitCode > 0) {
+		if (exitCode == 1) {
+			highlightAllActiveCells();
+			enableStart()
+		} else {
+			selectAllPossibleStartPointsWithUnevenEdges()
+			enableStart();
+		}
+	} else {
+		disableStart();
+	}
+}
+
+function selectAllPossibleStartPointsWithUnevenEdges(){
+	let active = getAllActiveCells();
+	active.forEach((a) => {
+		if(a.edges.size % 2 != 0)
+			a.setStartpoint();
+	});
+}
+
+function highlightAllActiveCells(){
+	let active = getAllActiveCells()
+	active.forEach(a =>{
+		if(a.edges.size > 0)
+		a.setStartpoint()
+	})
+}
 /**
  * Draws a grid and regigesters every field in the grid in a multidimensional array of grids.
  */
@@ -194,20 +289,45 @@ function drawGrid() {
 ctx.closePath();
 ctx.fillStyle = cellOpts.bg;
 ctx.strokeStyle= gridcolor
+let index = 0
 for (let i = 0; i < cells; i++) {
 	for (let j = 0; j < cells; j++) {
 		let currentX = cellOpts.width * i;
 		let currentY = cellOpts.height * j;
 		drawCell(currentX, currentY, cellOpts.bg, cellOpts.shadow);
 		if (grid[i][j] == null) {
-			grid[i][j] = cell(i, j);
+			grid[i][j] = cell(i, j,index++);
 		}
 	}
 }   
 }
 
+/**
+ * Draws the index of the cell in the center of a highlighted cell.
+ * @param {cell} cell the cell object
+ */
+function drawIndex(cell, color = cellOpts.bg, glow = cellOpts.shadow) {
+	ctx.closePath()
+	ctx.beginPath()
+	ctx.font =  cellOpts.width/3+"px arial";
+	ctx.fillStyle = color;
 
-function gradientLine(start, end){
+	ctx.shadowColor = glow;
+	ctx.shadowBlur = radius;
+	ctx.textAlign = "center";
+	ctx.textBaseline = "middle";
+	ctx.fillText(cell.index, cell.center.x, cell.center.y, cellOpts.width);
+	ctx.closePath();
+}
+
+
+/**
+ * Draws a gradientline from one point to the other
+ * @param {position} start start position x y
+ * @param {position} end end poisition x y
+ * @param {int} offset offset from x and y
+ */
+function gradientLine(start, end, offset = 0){
 
 	const gradient = ctx.createLinearGradient(
 		start.x,
@@ -215,18 +335,40 @@ function gradientLine(start, end){
 		end.x,
 		end.y
 	);
+		// calculate the offset from the center if the offset is not set to default 0
+	let p1 = {x:start.x, y:start.y}
+	let p2 = {x:end.x, y:end.y}
+
+	if(offset > 0){
+
+		dy = p2.y - p1.y
+		dx = p2.x - p1.x
+		let alpha = Math.atan2(dy,dx) 
+		debug(alpha)
+		
+		let xr,yr
+		xr = Math.cos(alpha)*offset
+		yr = Math.sin(alpha)*offset
+		// console.log("xr:",xr,"yr:", yr,"alpha:",alpha);
+		
+		p1.x += xr
+		p1.y += yr
+		p2.x -= xr
+		p2.y -= yr
+	}
+
 	gradient.addColorStop(0, gridcolor);
 	gradient.addColorStop(0.50, activeCellColor);
 	gradient.addColorStop(1, gridcolor);
 
 	ctx.strokeStyle = gradient;
-	ctx.lineWidth = 2;
-	
+	ctx.lineWidth = 1;
+	// todo: set offset according to target location in a given radius arround the center .
 	line(
-		start.x,
-		start.y , 
-		end.x ,
-		end.y 
+		p1.x,
+		p1.y, 
+		p2.x,
+		p2.y 
 	)
 }
 
@@ -264,6 +406,7 @@ function updateCanvas(){
 	ctx.fillStyle = gridcolor
 	drawGrid();
 	drawAllActiveCells();
+	highlightStartPoints();
 }
 
 /**
@@ -285,6 +428,8 @@ function line(fromX, fromY, toX, toY) {
  * Draw all active cells.
  */
 function drawAllActiveCells() {
+
+	let edges=[]
 	grid.forEach((row) => {
 		row.forEach(c=>{
 
@@ -292,7 +437,10 @@ function drawAllActiveCells() {
 				debug(c)
 				c.highlight()
 				c.edges.forEach((e) => {
-					gradientLine(c.center,e.center);
+					
+						gradientLine(c.center, e.center, offset);
+						edges.push();
+					
 				});
 			}
 		})
@@ -303,9 +451,13 @@ function drawAllActiveCells() {
  * Debug information if debugging is set to on
  * @param {*} o 
  */
-function debug(o){
-	if (isDebug) console.log(o);
-}
+function debug(...o){
+	if (isDebug) {
+		o.forEach(arg=>{
+
+			console.log(arg)
+		});
+}}
 
 
 function disableStart(){
@@ -318,27 +470,53 @@ function enableStart(){
 	btn.disabled = false;
 }
 
-function canStart(){
-	let active = []
-	grid.forEach(row=>{
-		row.forEach(c=>{
-			if (c.isActive)
-				active.push(c)
-		})
-	})
 
-	let maxEdgesEqual = 0
+/**
+ * Check all active nodes and count the edges. If all edges are equal or a maximum of 2 edges is dividable by 2 an euler circle is possible.
+ * @returns 
+ */
+function canStart(){
+	let active = getAllActiveCells()
+	
+	let equalEdges = 0
+	let edges = new Set()
 	active.forEach(c=>{
-		if(c.edges.length % 2 == 0){
-			maxEdgesEqual++
-		}else{
-			possibleStartpoints.push(c)
+		if(c.edges.size == 0)	// a cell has no edges and can not be visited
+			return 0
+		c.edges.forEach(e=>{
+			edges.add(e.index)
+		})
+		if(c.edges.size > 0 && c.edges.size % 2 == 0 ){
+			equalEdges++
 		}
-		if(maxEdgesEqual >2)
-		possibleStartpoints = []
-		return false
 	})
-	return true
+	// 1 best case all nodes are start points 2 only certain nodes are start points 0 no start points no circle possible
+	let exitCode
+	if (edges.size > 0 && equalEdges == edges.size){	// all edges are equal
+		exitCode = 1
+	}
+	else if (edges.size > 0 &&  equalEdges >= 2 ){
+		exitCode = 2
+	}
+	else {
+		exitCode = 0
+	}
+		// either all edges are equal or a max of 2 edges is equal to achive a valid euler circle
+	return exitCode		
+}
+
+/**
+ * Return all cells that are set to active.
+ * @returns array of cells
+ */
+function getAllActiveCells(){
+	let active =[];
+	grid.forEach((row) => {
+		row.forEach((c) => {
+			if (c.isActive) active.push(c);
+		});
+	});
+	return active
 }
 
 // ------------------------------------------
@@ -360,7 +538,10 @@ canvas.addEventListener(enter, function onEnter(e) {
 	});
 });
 
-
+/**
+ * Move Callback. Highlight the cell where the mouse entered.
+ * @param {event} e 
+ */
 function onMove(e) {
 	updateCanvas();
 	// fetch the current cell and highlight
@@ -372,11 +553,13 @@ function onMove(e) {
 		canvas.removeEventListener(move, onMove);
 	});
 }
+
 // global start for preview to be overridden
 let start = {
 	x: 0,
 	y: 0
 }
+
 /*
  Handle drag events and activating of grid cells to build the graph
 */
@@ -405,11 +588,16 @@ canvas.addEventListener(down, function connect(e) {
 	});
 });
 
+/**
+ * Draw a preview line on the canvas.
+ * @param {event} e 
+ */
 function preview(e){
 	updateCanvas()
 	ctx.beginPath();
+	// let pos = getGridCell(e).center
 	let pos = { x: e.x - rect.x, y: e.y - rect.y };
-	gradientLine(start, pos);
+	gradientLine(start, pos,offset);
 	ctx.closePath();
 }
 
@@ -430,6 +618,9 @@ function getGridCell(e) {
 	return grid[x][y];
 }
 
+/**
+ * Clear the whole canvas to start over.
+ */
 document.querySelector("#btn-clear").onclick = function onClearBtnPress() {
 	grid.forEach((row) => {
 		row.forEach((c) => {
@@ -442,12 +633,18 @@ document.querySelector("#btn-clear").onclick = function onClearBtnPress() {
 // ----------------------------------------
 //	TABLE OPTIONS
 // ----------------------------------------
+
+/**
+ * Update the table data.
+ */
 function updateTable() {
 	cellsForTable = [];
 
 	for (let i = 0; i < cells; i++) {
 		for (let j = 0; j < cells; j++) {
 			const cell = grid[i][j];
+			if (cell == null)
+				return
 			if (cell.isActive) {
 				let cellinfo = {
 					index: cell.index,
@@ -464,7 +661,10 @@ function updateTable() {
 	clearTable();
 	cellsForTable.forEach((cell) => appendRow(cell));
 }
-
+/**
+ * Appends a row with cell information to the table.
+ * @param {cell} cell 
+ */
 function appendRow(cell) {
 	let end = table.childNodes.length - 1;
 	let row = table.insertRow(end);
@@ -481,16 +681,22 @@ function appendRow(cell) {
 	edgeListCell.innerHTML = edges;
 }
 
+/**
+ * Clear all entries in the table but the header.
+ */
 function clearTable() {
 	var rows = table.rows;
 	var i = rows.length;
 	while (--i) {
 		rows[i].parentNode.removeChild(rows[i]);
-		// or
-		// table.deleteRow(i);
 	}
 }
 
+/**
+ * Convert the edges of the current cell to a string array.
+ * @param {*} edges the edges 
+ * @returns string of edges in form of {1,2,3}
+ */
 function stringFromSet(edges) {
 	let arr = []
 	edges.forEach(e=>{
